2f53a54ac30d4371dfe594d35b8c17504e3fb7bd
razzkumar <razzkumar.dev@gmail.com>
2025-10-31 17:47:01 +0545

    Rename package imaging to imgx and update module

--- examples/watermark_example.go
+++ examples/watermark_example.go
@@ -4,52 +4,52 @@ import (
 	"image/color"
 	"log"
 
-	"github.com/disintegration/imaging"
+	"github.com/razzkumar/imgx"
 )
 
 func main() {
 	// Open an image file
-	src, err := imaging.Open("input.jpg")
+	src, err := imgx.Open("input.jpg")
 	if err != nil {
 		log.Fatalf("failed to open image: %v", err)
 	}
 
 	// Example 1: Simple watermark with default settings
-	watermarked1 := imaging.Watermark(src, imaging.WatermarkOptions{
+	watermarked1 := imgx.Watermark(src, imgx.WatermarkOptions{
 		Text:    "Copyright 2025",
 		Opacity: 0.5,
 	})
 
 	// Save the watermarked image
-	err = imaging.Save(watermarked1, "output1.jpg")
+	err = imgx.Save(watermarked1, "output1.jpg")
 	if err != nil {
 		log.Fatalf("failed to save image: %v", err)
 	}
 
 	// Example 2: Custom watermark with position and color
-	watermarked2 := imaging.Watermark(src, imaging.WatermarkOptions{
+	watermarked2 := imgx.Watermark(src, imgx.WatermarkOptions{
 		Text:      "CONFIDENTIAL",
-		Position:  imaging.Center,
+		Position:  imgx.Center,
 		Opacity:   0.3,
 		TextColor: color.NRGBA{255, 0, 0, 255}, // Red text
 		Padding:   20,
 	})
 
-	err = imaging.Save(watermarked2, "output2.jpg")
+	err = imgx.Save(watermarked2, "output2.jpg")
 	if err != nil {
 		log.Fatalf("failed to save image: %v", err)
 	}
 
 	// Example 3: Top-left watermark with high opacity
-	watermarked3 := imaging.Watermark(src, imaging.WatermarkOptions{
+	watermarked3 := imgx.Watermark(src, imgx.WatermarkOptions{
 		Text:      "Sample Watermark",
-		Position:  imaging.TopLeft,
+		Position:  imgx.TopLeft,
 		Opacity:   0.8,
 		TextColor: color.White,
 		Padding:   10,
 	})
 
-	err = imaging.Save(watermarked3, "output3.jpg")
+	err = imgx.Save(watermarked3, "output3.jpg")
 	if err != nil {
 		log.Fatalf("failed to save image: %v", err)
 	}
